d8d7909b802dd013e7f343722fde5a617aa1a667
elifogreniyor <elfogreniyor@gmail.com>
2023-05-25 22:49:46 +0300

    question and answer added

--- lib/main.dart
+++ lib/main.dart
@@ -30,10 +30,20 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   
-  List<String> myStrings = ['a', 'b', 'c', 'd'];
   List<Widget> scoreKeeper = [
       ];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answers = [
+  false,
+  true,
+  true,
+  ];
 
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -45,8 +55,7 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
-              child: Text(
-                'This is where the question text will go.',
+              child: Text(questions[questionNumber ],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -69,11 +78,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionNumber];
+
+                if(correctAnswer == false){
+                  print('User got it right!');
+                } else(){
+                  print('User got it wrong!');
+                };
+
                 setState(() {
-                  scoreKeeper.add(Icon(
-                    Icons.check, color: Colors.green,
-                  ),
-                );
+                  questionNumber++;
               }
             );
           },
@@ -94,6 +108,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  questionNumber++;
                   scoreKeeper.add(Icon(
                     Icons.cancel,
                     color: Colors.red,
